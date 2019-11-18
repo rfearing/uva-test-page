67bec34c7b1b709dd18174518ce86b02c5100c8e
Ricardo Fearing <ricardo@Ricardos-MacBook-Pro.local>
2019-11-18 15:40:26 -0500

    Add ViewPort

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="author" content="Ricardo Fearing">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
     <link rel="stylesheet" href="./styles/style.css" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
     <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:300,300i,400,500,500i,700,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="shortcut icon" href="./images/favicon.ico" />
